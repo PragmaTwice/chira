f983ff941fc004f07a41c75031972cc0591cb5d3
PragmaTwice <twice@apache.org>
2025-06-17 13:18:31 +0800

    feat(sir): add self-recursive rewriter into recursive resolver pass

--- chira/dialect/sir/transforms/RecursiveResolver.cpp
+++ chira/dialect/sir/transforms/RecursiveResolver.cpp
@@ -14,21 +14,61 @@
 
 #include "chira/dialect/sir/SIROps.h"
 #include "chira/dialect/sir/transforms/Passes.h"
+#include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Diagnostics.h"
-#include "mlir/IR/Operation.h"
-#include "mlir/IR/Visitors.h"
+#include "mlir/IR/PatternMatch.h"
 #include "mlir/Transforms/DialectConversion.h"
-#include "llvm/Support/Casting.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 namespace chira::sir {
 
 namespace {
 
+struct ConvertSelfRecursive : mlir::OpRewritePattern<sir::ClosureOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(sir::ClosureOp op,
+                  mlir::PatternRewriter &rewriter) const override {
+    auto caps = op.getCaps();
+    auto it = std::find(caps.begin(), caps.end(), op.getRes());
+    if (it == caps.end()) {
+      return rewriter.notifyMatchFailure(op, "not a self-recursive closure");
+    }
+
+    auto lambda_type = sir::LambdaType::get(getContext());
+    auto i64 = mlir::IntegerType::get(getContext(), 64);
+    auto lambda = rewriter.create<sir::LambdaOp>(
+        op->getLoc(), lambda_type, mlir::IntegerAttr::get(i64, caps.size()));
+    lambda.getBody().takeBody(op.getBody());
+
+    auto var_type = sir::VarType::get(getContext());
+
+    std::vector<mlir::Value> new_caps;
+    for (auto cap : caps) {
+      new_caps.push_back(cap == op.getRes() ? lambda : cap);
+    }
+    rewriter.replaceOpWithNewOp<sir::BindOp>(
+        op, var_type, mlir::ValueRange{lambda}, new_caps);
+    return mlir::success();
+  }
+};
+
 struct RecursiveResolverPass
     : public mlir::PassWrapper<RecursiveResolverPass,
                                mlir::OperationPass<mlir::ModuleOp>> {
-  void runOnOperation() override {}
+  void runOnOperation() override {
+    mlir::MLIRContext *context = &getContext();
+    mlir::ModuleOp module = getOperation();
+
+    mlir::RewritePatternSet patterns(context);
+    patterns.add<ConvertSelfRecursive>(context);
+
+    if (failed(mlir::applyPatternsGreedily(module, std::move(patterns))))
+      signalPassFailure();
+  }
 };
 
 } // namespace
